c52e37eeee5f343d4912ca3077ecb2749a0ff208
chikaskas <rivaandesai3219@gmail.com>
2024-11-08 20:58:09 +0700

    Added piece move logic.
    And colors

--- main/main.ino
+++ main/main.ino
@@ -4,10 +4,12 @@
 //pins
 extern const uint8_t start_button_pin;
 extern const uint8_t led_strip_pin;
+extern const uint32_t led_count;
+extern const uint8_t led_positions[8][8];
 extern const uint8_t reed_switch_pins[8][8];
 
 //other variables
-volatile Adafruit_NeoPixel led_strip = Adafruit_NeoPixel(64, led_strip_pin, NEO_GRB + NEO_KHZ800);
+extern volatile Adafruit_NeoPixel led_strip;
 volatile int8_t chess_board[8][8] = {//Initialize chess board
   /*
   Chess board encoding
@@ -25,6 +27,13 @@ volatile int8_t chess_board[8][8] = {//Initialize chess board
   {-1,-1,-1,-1,-1,-1,-1,-1,},//7.   Black pawns
   {-2,-3,-4,-5,-6,-4,-3,-2,},//8.   White side
 };
+
+//colors
+extern const uint32_t color_black;
+extern const uint32_t color_red;
+extern const uint32_t color_green;
+extern const uint32_t color_blue;
+extern const uint32_t color_white;
 
 void setup() {
   Serial.begin(9600);
@@ -55,6 +64,15 @@ volatile bool prev_readings[8][8] = {
   {1, 1, 1, 1, 1, 1, 1, 1},
 };
 
+volatile bool piece_up = false;
+volatile uint8_t picked_piece_row = 0;
+volatile uint8_t picked_piece_column = 0;
+
+volatile uint8_t last_move_from_row = 0;
+volatile uint8_t last_move_from_column = 0;
+volatile uint8_t last_move_to_row = 0;
+volatile uint8_t last_move_to_column = 0;
+
 void loop() {
   delay(100);
 
@@ -66,17 +84,35 @@ void loop() {
       volatile bool prev_read = digitalRead(reed_switch_pins[row][column]);
       volatile bool cur_read = digitalRead(reed_switch_pins[row][column]);
 
-      if (prev_read && !cur_read) {
+      if (prev_read && !cur_read && !piece_up) {
+        piece_up = true;
+        picked_piece_row = row;
+        picked_piece_column = column;
+        led_strip.setPixelColor(led_positions[row][column], color_blue);
+        {}//calc and display moves
         //piece lifted
       }//piece lifted
 
       if (cur_read && !prev_read) {
+
+        piece_up = false;
+
+        led_strip.setPixelColor(led_positions[last_move_from_row][last_move_from_column], color_black);
+        led_strip.setPixelColor(led_positions[last_move_to_row][last_move_to_column], color_black);
+        led_strip.setPixelColor(led_positions[row][column], color_blue);
+
+        last_move_from_row = picked_piece_row;
+        last_move_from_column = picked_piece_column;
+        last_move_to_row = row;
+        last_move_to_column = column;
+
         //piece placed
       }//piece placed
 
     }
   }
 
+  led_strip.show();
 }
 
 void loop_through_squares(void (*func)(uint8_t row, uint8_t column)) {
--- main/settings.ino
+++ main/settings.ino
@@ -4,3 +4,11 @@ const uint8_t led_strip_pin = 3;
 const uint32_t led_count = 121;
 const uint8_t led_positions[8][8] = {0};
 const uint8_t reed_switch_pins[8][8] = {0};
+
+//colors
+volatile Adafruit_NeoPixel led_strip = Adafruit_NeoPixel(64, led_strip_pin, NEO_GRB + NEO_KHZ800);
+const uint32_t color_black = led_strip.Color(0, 0, 0);
+const uint32_t color_red = led_strip.Color(255, 0, 0);
+const uint32_t color_green = led_strip.Color(0, 255, 0);
+const uint32_t color_blue = led_strip.Color(0, 0, 255);
+const uint32_t color_white = led_strip.Color(255, 255, 255);
